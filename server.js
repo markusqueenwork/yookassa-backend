b5af1dac24479a86869def231f380e8c82404ca1
markusqueenwork <markusqueenwork@gmail.com>
2026-04-15 04:31:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
+app.listen(PORT, '0.0.0.0', async () => {
   console.log(`🚀 Бэкенд запущен на порту ${PORT}`);
   await initDB();
 });
